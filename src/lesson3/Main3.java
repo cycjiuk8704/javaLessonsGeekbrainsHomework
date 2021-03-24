44ea043d334beb19c763e5e243591e724c929605
cycjiuk8704 <cycjiuk8704@gmail.com>
2021-03-24 20:19:23 +0300

    Homework 3 edited

--- src/lesson3/Main3.java
+++ src/lesson3/Main3.java
@@ -14,16 +14,17 @@ public class Main3 {
     public static void guessWord() {
         String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi",
                 "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
-        String answer = words[(int) (Math.random() * 25)];
+        String answer = words[(int) (Math.random() * words.length)];
         System.out.println("Попробуйте угадать слово");
         String userAnswer = scanner.nextLine();
+        char[] hint = {'*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*'};
         for (int i = 1; i <= answer.length(); i++) {
 
-            char[] hint = {'*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*'};
             if (i == answer.length()) {
                 System.out.println("Вы проиграли. Загаданное слово - " + answer);
             } else if (userAnswer.equals(answer)) {
                 System.out.println("Вы угадали");
+                break;
             } else {
                 answer.getChars(0, i, hint, 0);
                 System.out.println("Вы ввели неправильное слово. Попробуйте еще раз. Подсказка: " + Arrays.toString(hint));
