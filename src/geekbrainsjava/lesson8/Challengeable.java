6ac9b2947cc54cbc7f7de9a12c5929e21b166e2d
cycjiuk8704 <cycjiuk8704@gmail.com>
2021-04-16 22:17:36 +0300

    Homework 8

--- src/geekbrainsjava/lesson8/Challengeable.java
+++ src/geekbrainsjava/lesson8/Challengeable.java
@@ -1,10 +1,7 @@
 package geekbrainsjava.lesson8;
 
-import geekbrainsjava.lesson8.obstacles.ObstacleDirection;
-
 public interface Challengeable {
 
-//    void passObstacle(ObstacleDirection obstacleDirection);
     boolean passObstacle(Overcomeable overcomeable);
     int getMaxRunDistance();
     int getMaxClimbDistance();
