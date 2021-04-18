0c26a0c0b871ebbde9d97e51775f4ba095070d1c
cycjiuk8704 <cycjiuk8704@gmail.com>
2021-04-18 16:45:55 +0300

    Homework 9

---
+++ src/geekbrainsjava/lesson9/ArrayChecker.java
@@ -0,0 +1,23 @@
+package geekbrainsjava.lesson9;
+
+public class ArrayChecker {
+    public static int sumArrayElements(String[][] arrayToSum) throws MyArrayDataException, MyArraySizeException {
+        int sum = 0;
+        if (arrayToSum.length != 4 || arrayToSum[1].length != 4) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < arrayToSum.length; i++) {
+            for (int j = 0; j < arrayToSum[1].length; j++) {
+                try {
+                    sum += Integer.parseInt(arrayToSum[i][j]);
+                }
+                catch (NumberFormatException nfe) {
+                    throw new MyArrayDataException(i, j, arrayToSum[i][j], nfe);
+                }
+            }
+        }
+        return sum;
+    }
+}
+
+
---
+++ src/geekbrainsjava/lesson9/MyArrayDataException.java
@@ -0,0 +1,9 @@
+package geekbrainsjava.lesson9;
+
+public class MyArrayDataException extends Exception{
+
+    public MyArrayDataException (int i, int j, String symb, NumberFormatException nfe) {
+        super("Invalid data '" + symb + "' at array element [" + i + "][" + j +"]");
+    }
+
+}
