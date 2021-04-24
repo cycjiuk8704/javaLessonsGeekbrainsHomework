3c641c7714bf4b79d6406c204499dff9e0801e23
cycjiuk8704 <cycjiuk8704@gmail.com>
2021-04-24 23:35:44 +0300

    Homework11

--- src/geekbrainsjava/lesson11/PartOneAndTwo/ArrayUtils.java
+++ src/geekbrainsjava/lesson11/PartOneAndTwo/ArrayUtils.java
@@ -6,12 +6,9 @@ import java.util.Collections;
 public class ArrayUtils<T> {
 
     public static <T> void swapElements(T[] objArr, int element1, int element2) {
-
-
         T buf = objArr[element1];
         objArr[element1] = objArr[element2];
         objArr[element2] = buf;
-
     }
 
     public static <T> ArrayList<T> transformToList(T[] objArr) {
