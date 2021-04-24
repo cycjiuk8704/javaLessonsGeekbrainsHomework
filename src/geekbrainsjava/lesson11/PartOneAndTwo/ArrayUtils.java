a1b1b1f8a3b2f8e2a27d18ec63cb694f7801aa28
cycjiuk8704 <cycjiuk8704@gmail.com>
2021-04-24 23:10:36 +0300

    Homework11

--- src/geekbrainsjava/lesson11/PartOneAndTwo/ArrayUtils.java
+++ src/geekbrainsjava/lesson11/PartOneAndTwo/ArrayUtils.java
@@ -5,13 +5,13 @@ import java.util.Collections;
 
 public class ArrayUtils<T> {
 
-    public void swapElements(T[] objArr, int element1, int element2) {
+    public static <T> void swapElements(T[] objArr, int element1, int element2) {
         T buf = objArr[element1];
         objArr[element1] = objArr[element2];
         objArr[element2] = buf;
     }
 
-    public ArrayList<T> transformToList(T[] objArr) {
+    public static <T> ArrayList<T> transformToList(T[] objArr) {
         ArrayList<T> objArrList = new ArrayList<>();
         Collections.addAll(objArrList, objArr);
         return objArrList;
