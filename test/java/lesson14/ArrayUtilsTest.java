f4ab4da8ea1bac12498813a36433cd4a150a477c
cycjiuk8704 <cycjiuk8704@gmail.com>
2021-05-07 17:01:19 +0300

    Homework 14

--- test/java/lesson14/ArrayUtilsTest.java
+++ test/java/lesson14/ArrayUtilsTest.java
@@ -26,7 +26,9 @@ public class ArrayUtilsTest {
     public void testModifyArr3() {
         int[] expectedArray = new int[]{0};
         int[] actualArray = new int[]{1, 3, 7, 3, 5, 3, 42, 1, 2, 1};
-        Assertions.assertArrayEquals(expectedArray, ArrayUtils.arrModifier(actualArray));
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            ArrayUtils.arrModifier(actualArray);
+        });
     }
 
 
